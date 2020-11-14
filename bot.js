028b4d45bd32c49c8e6e1d82d43de782561ed60c
brendonbeyer10 <36339515+brendonbeyer10@users.noreply.github.com>
2020-11-13 18:51:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,57 @@
-var http, director, cool, bot, router, server, port;
+var HTTPS = require('https');
+var cool = require('cool-ascii-faces');
 
-http        = require('http');
-director    = require('director');
-cool        = require('cool-ascii-faces');
-bot         = require('./bot.js');
+var botID = process.env.BOT_ID;
 
-router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
+function respond() {
+  var request = JSON.parse(this.req.chunks[0]),
+      botRegex = /^\/cool guy$/;
+
+  if(request.text && botRegex.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage();
+    this.res.end();
+  } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
   }
-});
-
-server = http.createServer(function (req, res) {
-  req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
-  });
-
-  router.dispatch(req, res, function(err) {
-    res.writeHead(err.status, {"Content-Type": "text/plain"});
-    res.end(err.message);
-  });
-});
-
-port = Number(process.env.PORT || 5000);
-server.listen(port);
-
-function ping() {
-  this.res.writeHead(200);
-  this.res.end("Hey, I'm Cool Guy.");
 }
+
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = cool();
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+exports.respond = respond;
