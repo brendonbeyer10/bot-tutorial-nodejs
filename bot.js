dfed2437fa6c9fd926a4d601042b893bad4039ef
brendonbeyer10 <36339515+brendonbeyer10@users.noreply.github.com>
2021-01-05 22:01:22 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0])
-  if(request.name == 'Brendon Beyer1'){ // == 'Photoshop God'
+  if(request.name == 'Photoshop God'){ // == 'Photoshop God'
         postMessage(request) 
   }
 }
