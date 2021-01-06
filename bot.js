52fd857d2a013d9ccb07e9c4a62ac6e2ca019634
brendonbeyer10 <36339515+brendonbeyer10@users.noreply.github.com>
2021-01-05 21:35:30 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ function postMessage(request) {
   var botResponse, options, body, botReq;
   
   //var resp1 = 'original text: ' + request.text + '. @Jake Joe wants you. Threshold: ' + twss.prob(request.text)
-  var resp1 = request.text
+  var resp1 = request.text + '1jhg';
   botResponse = resp1
 
   options = {
